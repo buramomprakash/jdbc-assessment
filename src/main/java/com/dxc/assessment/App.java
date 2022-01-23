d2bd6427db2aa79c10f5a590bf0e51a56453f4a2
buramomprakash <96856567+buramomprakash@users.noreply.github.com>
2022-01-24 01:29:53 +0530

    Update App.java

--- src/main/java/com/dxc/assessment/App.java
+++ src/main/java/com/dxc/assessment/App.java
@@ -1,13 +1,56 @@
 package com.dxc.assessment;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import com.dxc.assessment.dao.AuthorDao;
+import com.dxc.assessment.dao.AuthorDaoImpl;
+import com.dxc.assessment.modal.Author;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        findAllAuthors();
+    }
+
+    static void findAllAuthors(){
+       
+        AuthorDao authorDao= new AuthorDaoImpl();
+        try {
+            List<Author> authors = authorDao.findAll();
+            if (authors.size() == 0) {
+                System.out.println("No Authors found");
+                return;
+            }
+            authors.forEach(as -> System.out.println(as));
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.err.println(e.getMessage());
+        }
+
+    }
+
+    static void createAuthors() {
+        Author  george=new Author("george","david","comics","georged@gmail.com");
+        Author  trivikram=new Author("trivikram","srinivas","novels","tsrinu@gmail.com");
+        Author  raja=new Author("raja","mouli","fictional","rrr@gmail.com");
+        Author  boyapati=new Author("boyapati","srinu","adventerous","bs@gmail.com");
+        
+       
+        List<Author> authors = List.of(george,trivikram,raja,boyapati);
+        AuthorDao authorDao = new AuthorDaoImpl();
+
+        authors.forEach(as -> {
+            try {
+                int rows = AuthorDao.save(as);
+                System.out.println("Rows affected: " + rows);
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.err.println(e.getMessage());
+            }
+        });
     }
 }
