57214ef7de2733342b600e73dbed7e1848ed4c91
buramomprakash <96856567+buramomprakash@users.noreply.github.com>
2022-01-24 01:28:50 +0530

    Update AuthorDao.java

--- src/main/java/com/dxc/assessment/dao/AuthorDao.java
+++ src/main/java/com/dxc/assessment/dao/AuthorDao.java
@@ -1,16 +1,21 @@
 package com.dxc.assessment.dao;
 
+import java.sql.SQLException;
 import java.util.List;
+
 import com.dxc.assessment.modal.Author;
 
-public interface AuthorDao {
+public interface AuthorDao  {
 
-    Author create(Author author);
+    Author create(Author author) throws SQLException;
 
-    Author findById(Long id);
+    Author findById(Long id) throws SQLException;
     
-    List<Author> findByGenre(String genre);
+    List<Author> findByGenre(String genre) throws SQLException;
 
-    List<Author> findAll();
-    
+    List<Author> findAll() throws SQLException;
+
+    static int save(Author as) {
+        return 0;
+    }
 }
